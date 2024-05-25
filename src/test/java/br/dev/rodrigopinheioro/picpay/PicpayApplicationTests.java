97a82c4511863b0fbc19bc9bdf2e6aeec1d63c0a
rpsouza441 <rpsouza@hotmail.co.uk>
2024-05-25 14:27:46 -0300

    Primeiro commit

---
+++ src/test/java/br/dev/rodrigopinheioro/picpay/PicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigopinheioro.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
